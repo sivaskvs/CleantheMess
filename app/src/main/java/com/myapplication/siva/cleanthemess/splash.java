ff8f6a71cb4741bdd3f4ebdbf5a82ec946234325
Siva subramanian <kvslsiva@yahoo.com>
2016-04-10 14:43:03 +0530

    Kriya
    Clean the mess for Kriya,Psg tech

---
+++ app/src/main/java/com/myapplication/siva/cleanthemess/splash.java
@@ -0,0 +1,56 @@
+package com.myapplication.siva.cleanthemess;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v4.view.ViewPager;
+
+public class splash extends FragmentActivity implements ActionBar.TabListener {
+
+    private ViewPager viewPager;
+    private TabsPagerAdapter mAdapter;
+   // private ActionBar actionBar;
+    // Tab titles
+    private String[] tabs = {"Top Rated", "Games", "Movies"};
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Initilization
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        //actionBar = getActionBar();
+        mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(mAdapter);
+     /*   actionBar.setHomeButtonEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Adding Tabs
+       for (String tab_name : tabs) {
+            actionBar.addTab(actionBar.newTab().setText(tab_name)
+                    .setTabListener(this));
+        }*/
+
+    }
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+}
